1c9c583c734df32857345fd6d64d2c98eb232349
Kazuhisa Kawashima <cupper@mac.com>
2020-02-22 16:30:38 +0900

    Test Commit!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Private Note</router-link> |
+      <router-link to="/">Private Note hogehoge</router-link> |
       <router-link to="/signin">Sign In</router-link> |
       <router-link to="/about">About</router-link>
     </div>
